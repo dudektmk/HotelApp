e6a586431ba6286be2933d98ff9aa7c93a24c5de
woodripper <k.gornicki@op.pl>
2015-02-10 15:53:24 +0100

    Merging master with validation

--- src/main/java/com/luxoft/servlets/AddBookedServlet.java
+++ src/main/java/com/luxoft/servlets/AddBookedServlet.java
@@ -1,13 +1,24 @@
 package com.luxoft.servlets;
 
+
+
+
 import com.luxoft.service.RoomReservationsService;
 import com.luxoft.service.RoomReservationsServiceImpl;
+import com.luxoft.validator.Validator;
+import com.sun.istack.internal.NotNull;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.validation.annotation.Validated;
+import org.thymeleaf.util.Validate;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.HashSet;
 
 /**
  * Created by Dudi on 2015-02-09.
@@ -15,13 +26,23 @@ import java.io.IOException;
 public class AddBookedServlet extends HttpServlet {
 
     //@Autowired
-    private RoomReservationsService roomReservationsService=RoomReservationsServiceImpl.getInstance();
+    private RoomReservationsService roomReservationsService= RoomReservationsServiceImpl.getInstance();
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        roomReservationsService.addReservation((String)req.getParameter("roomId"));
+        if (Validator.validateRoomId(req.getParameter("roomId"))) {
+            roomReservationsService.addReservation((String)req.getParameter("roomId"));
+            req.setAttribute("bookedRooms", roomReservationsService.getReservations());
+            req.getRequestDispatcher("/").forward(req, resp);
+    } else {
+        req.setAttribute("roomError", "true");
         req.setAttribute("bookedRooms", roomReservationsService.getReservations());
         req.getRequestDispatcher("/").forward(req, resp);
     }
+    }
+
+//    public void setHotelService(HotelService hotelService) {
+//        this.hotelService = hotelService;
+//    }
 }
