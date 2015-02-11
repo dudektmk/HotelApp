c49120e1bc4ce5c73df091c804174cc597807d75
tdudek <dudek.tmk@gmail.com>
2015-02-11 10:59:32 +0100

    adding test for validator

---
+++ src/test/java/com/luxoft/model/ValidatorTest.java
@@ -0,0 +1,39 @@
+package com.luxoft.model;
+
+import com.luxoft.service.RoomReservationsService;
+import com.luxoft.service.RoomReservationsServiceImpl;
+import com.luxoft.validator.Validator;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by tdudek on 2015-02-11.
+ */
+public class ValidatorTest {
+
+    private RoomReservationsService roomReservationsService=RoomReservationsServiceImpl.getInstance();;
+
+    @Before
+    public void init(){
+        roomReservationsService.cancelAll();
+    }
+    @Test
+    public void shouldReturnEmptyIdWhenNoImputProvided() throws Exception {
+        Assert.assertEquals("emptyId", Validator.validateAndAddIfOk("", roomReservationsService));
+    }
+
+    @Test
+    public void shouldReturnWrongIdWhenNoProperInput() throws Exception {
+        Assert.assertEquals("wrongId", Validator.validateAndAddIfOk("!",roomReservationsService));
+    }
+    @Test
+    public void shouldReturnAlreadyBookedWhenTryingToDuplicateReservation() throws Exception {
+        roomReservationsService.addReservation("room1");
+        Assert.assertEquals("alreadyBooked", Validator.validateAndAddIfOk("room1",roomReservationsService));
+    }
+    @Test
+    public void shouldReturnEmptyStringWhenProperInput() throws Exception {
+        Assert.assertEquals("", Validator.validateAndAddIfOk("room1",roomReservationsService));
+    }
+}
