44595672034b4d6c25e0ba19da4c3ee6420e6a04
tdudek <dudek.tmk@gmail.com>
2015-02-13 14:26:49 +0100

    REFACTORING - Reformat code

--- src/main/java/com/luxoft/servlets/AddBookedServlet.java
+++ src/main/java/com/luxoft/servlets/AddBookedServlet.java
@@ -1,5 +1,5 @@
 package com.luxoft.servlets;
-import com.luxoft.exceptions.ReservationExistsException;
+
 import com.luxoft.service.RoomReservationsService;
 import com.luxoft.service.RoomReservationsServiceImpl;
 import com.luxoft.validator.Validator;
@@ -10,19 +10,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * Created by Dudi on 2015-02-09.
- */
 public class AddBookedServlet extends HttpServlet {
 
     //@Autowired
-    private RoomReservationsService roomReservationsService= RoomReservationsServiceImpl.getInstance();
+    private RoomReservationsService roomReservationsService = RoomReservationsServiceImpl.getInstance();
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
-        req.setAttribute("roomError", Validator.validateAndAddIfOk(req.getParameter("roomId"),req.getParameter("clientName"), roomReservationsService));
+        req.setAttribute("roomError", Validator.validateAndAddIfOk(req.getParameter("roomId"), req.getParameter("clientName"), roomReservationsService));
         req.setAttribute("bookedRooms", roomReservationsService.getReservations());
         req.getRequestDispatcher("/reservation").forward(req, resp);
     }
